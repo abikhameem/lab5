00a7dce9b9340aa710a5b6ab2be113866eec5a6e
abikhameem <42241025+abikhameem@users.noreply.github.com>
2018-08-30 16:02:30 -0400

    Add files via upload

---
+++ lab5_q6.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	 int a;
+	cout<<" give the year a"<<endl;
+	cin>>a;
+	if((2008-a)%4==0){cout<<"the given year a"<<a<<"is a leap year";}
+        else{cout<<"the given year a"<<a<<"is not a leap year";}
+return 0;
+}
