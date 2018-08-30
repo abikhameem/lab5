f7e67482bdd05d2ea287a778c3eccf01dcaf136d
abikhameem <42241025+abikhameem@users.noreply.github.com>
2018-08-30 15:45:25 -0400

    Add files via upload

---
+++ lab5_q5.cpp
@@ -0,0 +1,18 @@
+//load the library
+#include<iostream>
+using namespace std;
+//using main function
+int main(){
+	//declaring variable
+	int a;
+	cout<<"define a="<<endl;
+	//giving value for a
+	cin>>a;
+	//starting if operation
+	if (a%2==0){cout<<"the number a="<<a<<"is even";}
+	//starting else operation if above condition is not valid 
+	else{cout<<"the number a="<<a<<" is odd";}
+//closing program
+return 0;
+}
+	
