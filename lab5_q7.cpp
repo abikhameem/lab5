b7f0945c17853c3de7f4bea1c68f4c22fe0b2ecd
abikhameem <42241025+abikhameem@users.noreply.github.com>
2018-08-30 16:35:13 -0400

    Add files via upload

---
+++ lab5_q7.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char ch;
+	cout<<"give any character ";
+	cin>>ch;
+	if (ch>='a'&& ch<='z'){cout<<"the given character "<<ch<<"an alphabet";}
+	else{cout<<"the given character "<<ch<<"not an alphabet";}
+return 0;
+} 
