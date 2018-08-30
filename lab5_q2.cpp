54716d7ea4af8ae3f998190d2db6db2955bccecf
abikhameem <42241025+abikhameem@users.noreply.github.com>
2018-08-30 14:15:52 -0400

    Add files via upload

---
+++ lab5_q2.cpp
@@ -0,0 +1,25 @@
+//load the library
+#include<iostream>
+using namespace std;
+//using main function
+int main(){
+	//declaring variables
+        float a,b,c;
+	cout<<"first number a="<<endl;
+	//giving value for a
+	cin>>a;
+	cout<<"second number b="<<endl;
+	//giving value for b
+	cin>>b;
+	cout<<"third number c="<<endl;
+	//giving value for c
+	cin>>c;
+	//using 'if' operation
+	if(a>b & a>c){cout<<"first number a="<<a<<"is the largest";}
+	//using 'else if' operation since above condition is not valid
+	else if (b>a & b>c){cout<<"second number b="<<b<<"is the largest";} 
+	//using 'else if' operation when above two condition is not valid
+        else if (c>a & c>b){cout<<"third number c="<<c<<"is the largest";}
+//closing the program
+return 0;
+} 
