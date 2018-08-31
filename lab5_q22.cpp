af53b65c00f590ac65332eb282a890c0e92eff16
abikhameem <42241025+abikhameem@users.noreply.github.com>
2018-08-31 16:34:20 -0400

    Add files via upload

---
+++ lab5_q22.cpp
@@ -0,0 +1,17 @@
+//load the library
+#include<iostream>
+using namespace std;
+// using main function
+int main(){
+           //declaring variable
+           int i=1,n;
+           cout<<"give value for n";
+           cin>>n;
+           
+           //looping all natural nos from i to n
+           while (i<=n){cout <<i<<endl;
+                        i++;
+                        }
+           //closing program
+           return 0;
+}
