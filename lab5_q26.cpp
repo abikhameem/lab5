be09adeafeeb668a3983981c646cf11fa23f72ff
abikhameem <42241025+abikhameem@users.noreply.github.com>
2018-08-31 16:47:50 -0400

    Add files via upload

---
+++ lab5_q26.cpp
@@ -0,0 +1,16 @@
+//load the library
+#include<iostream>
+using namespace std;
+//main fn
+int main(){
+           //declaring variable i
+           int i=1;
+           //looping all odd numbers from i to 100
+           while (i<=100){cout <<i<<endl;
+                        i+=2;
+                 //take i to the next odd no for the next loop
+                        }
+//closing the program
+return 0;
+}
+
