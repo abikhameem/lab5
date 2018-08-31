7ff3ebf2eb54e02d59724bde5c18cfb9bf898fd1
abikhameem <42241025+abikhameem@users.noreply.github.com>
2018-08-31 17:01:11 -0400

    Add files via upload

---
+++ lab5_q24.cpp
@@ -0,0 +1,14 @@
+//load the library
+#include<iostream>
+using namespace std;
+//using main function
+int main(){
+	//declaring variable
+	char i='a';
+	//looping a to z
+	while(i>='a' && i<='z'){cout<<i<<endl;
+	i++;
+	}
+//closing the program
+return 0;
+}
